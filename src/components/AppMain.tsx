eb0a5acba5b5bf50d5151cdb53bbecf9d8d6e140
dblatcher <dv.bltchr@gmail.com>
2023-07-05 21:25:02 +0100

    simple control

--- src/components/AppMain.tsx
+++ src/components/AppMain.tsx
@@ -1,32 +1,17 @@
 'use client'
 
-import { Box, Button, Container, Dialog, DialogContent, Typography } from "@mui/material"
+import { Box, Button, Container, Dialog, DialogContent, Grid, Typography } from "@mui/material"
 import { SvgFrame } from "./SvgFrame"
 import { useState } from "react"
-import { Unit, UnitDesign } from "@/types"
+import { Position, Unit, UnitDesign } from "@/types"
 import { RectangularUnit } from "./RectangularUnit"
 import { UnitDesigner } from "./UnitDesigner"
+import { UnitControl } from "./UnitControl"
 
 export const AppMain = () => {
     const [unitDesignerOpen, setUnitDesignerOpen] = useState(false)
     const [units, setUnits] = useState<Unit[]>([
-        {
-            x: 45,
-            y: 40,
-            width: 20,
-            height: 20,
-            heading: 1,
-            col1: "green",
-            col2: "white",
-            patternShape: "right-diagonal",
-        }
     ])
-
-    const doSomething = () => {
-        setUnits([
-            { ...units[0], heading: (units[0].heading || 0) + .1 }
-        ])
-    }
 
     const handleConfirmDesign = (unit: UnitDesign) => {
         setUnitDesignerOpen(false)
@@ -34,25 +19,41 @@ export const AppMain = () => {
             ...units,
             {
                 ...unit,
-                x: Math.floor(Math.random() * 100), 
-                y: Math.floor(Math.random() * 100), 
-                heading: .1
+                x: Math.floor(Math.random() * 100),
+                y: Math.floor(Math.random() * 100),
+                heading: 0,
             }
+        ])
+    }
+
+    const handleMove = (position: Position, index: number) => {
+        const original = units[index]
+        const changed = {
+            ...original, ...position
+        }
+        setUnits([
+            ...units.slice(0, index),
+            changed,
+            ...units.slice(index + 1)
         ])
     }
 
     return (
         <Container maxWidth={'lg'}>
             <Typography variant="h2">app</Typography>
-
-            <Box>
-                <Button onClick={() => { setUnitDesignerOpen(true) }}>add unit</Button>
-            </Box>
-
-            <SvgFrame style={{ backgroundColor: 'lightgreen', maxWidth: '500px' }}>
-                {units.map((unit, index) => <RectangularUnit key={index} {...unit} />)}
-            </SvgFrame>
-            <Button onClick={doSomething}>do something</Button>
+            <Grid container>
+                <Grid item xs={8}>
+                    <SvgFrame style={{ backgroundColor: 'lightgreen' }}>
+                        {units.map((unit, index) => <RectangularUnit key={index} {...unit} />)}
+                    </SvgFrame>
+                </Grid>
+                <Grid item xs={4}>
+                    {units.map((unit, index) => <UnitControl key={index} unit={unit} move={(position) => { handleMove(position, index) }} />)}
+                    <Box>
+                        <Button variant="outlined" onClick={() => { setUnitDesignerOpen(true) }}>add unit</Button>
+                    </Box>
+                </Grid>
+            </Grid>
 
             <Dialog open={unitDesignerOpen}>
                 <DialogContent>
---
+++ src/components/UnitControl.tsx
@@ -0,0 +1,37 @@
+import { Position, Unit } from "@/types";
+import { Box, Button } from "@mui/material";
+
+type Props = { unit: Unit, move: { (position: Position): void } }
+
+export const UnitControl = ({ unit, move }: Props) => {
+
+    const up = () => {
+        move({ ...unit, y: unit.y - 5 })
+    }
+    const down = () => {
+        move({ ...unit, y: unit.y + 5 })
+    }
+    const left = () => {
+        move({ ...unit, x: unit.x - 5 })
+    }
+    const right = () => {
+        move({ ...unit, x: unit.x + 5 })
+    }
+    const clockwise = () => {
+        move({ ...unit, heading: (unit.heading ?? 0) - .1 })
+    }
+    const antiClockwise = () => {
+        move({ ...unit, heading: (unit.heading ?? 0) + .1 })
+    }
+
+    return (
+        <Box>
+            <p>[{unit.x}, {unit.y}] {unit.heading?.toFixed(1)}</p>
+            <Button size="small" onClick={up}>&uarr;</Button>
+            <Button size="small" onClick={down}>&darr;</Button>
+            <Button size="small" onClick={left}>&larr;</Button>
+            <Button size="small" onClick={antiClockwise}>↺</Button>
+            <Button size="small" onClick={clockwise}>↻</Button>
+        </Box>
+    )
+}
