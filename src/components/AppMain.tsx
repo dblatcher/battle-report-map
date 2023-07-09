bed0d0f1227266955fa8d1b1588801ea9bd8e878
dblatcher <dv.bltchr@gmail.com>
2023-07-09 19:34:10 +0100

    layout stuff

--- src/components/AppMain.tsx
+++ src/components/AppMain.tsx
@@ -1,6 +1,6 @@
 import { useArrayState } from "@/lib/useArrayState"
-import { Badge, BattleField, Position, Unit, UnitDesign, ViewBox } from "@/types"
-import { Box, Button, Container, Dialog, DialogContent, Grid, Typography } from "@mui/material"
+import { Badge, BattleField, Position, Unit, UnitDesign } from "@/types"
+import { AppBar, Box, Button, Container, Dialog, DialogActions, DialogContent, DialogTitle, Grid, Toolbar, Typography } from "@mui/material"
 import { useState } from "react"
 import { DownloadableSvgFrame } from "./DownloadableSvgFrame"
 import { UnitFigure } from "./UnitFigure"
@@ -12,12 +12,13 @@ import { defaultBadges } from "@/lib/badges"
 
 export const AppMain = () => {
     const [unitDesignerOpen, setUnitDesignerOpen] = useState(false)
+    const [battleFieldDesignerOpen, setBattleFieldDesignerOpen] = useState(true)
     const [badges, setBadges, badgeArry] = useArrayState<Badge>(defaultBadges)
     const [battleField, setBattleField] = useState<BattleField>({
         viewBox: { width: 300, height: 200 },
         backgroundColor: '#44AA33'
     })
-    
+
     const [units, setUnits, unitArray] = useArrayState<Unit>([])
     const [activeUnitIndex, setActiveUnitIndex] = useState<number | undefined>(undefined)
 
@@ -45,51 +46,74 @@ export const AppMain = () => {
     }
 
     return (
-        <Container maxWidth={'lg'}>
-            <Typography variant="h2">app</Typography>
-            <Grid container>
-                <Grid item xs={8}>
-                    <DownloadableSvgFrame
-                        reportClick={handleFrameClick}
-                        fileName="map.png" boxProps={{ border: '1px solid black', padding: 1 }} viewBox={battleField.viewBox}>
-                        <FloodRect fill={battleField.backgroundColor} viewBox={battleField.viewBox} />
-                        {units.map((unit, index) => (
-                            <UnitFigure key={index}
-                                isActive={activeUnitIndex === index}
-                                showMarkers
-                                unit={unit} />
-                        ))}
-                    </DownloadableSvgFrame>
-                </Grid>
-                <Grid item xs={4} paddingX={1}>
-                    <BattleFieldDesigner
-                        battleField={battleField}
-                        setBattleField={setBattleField} />
-                    {units.map((unit, index) => (
-                        <UnitControl
-                            key={index}
-                            unit={unit}
-                            index={index}
-                            activeIndex={activeUnitIndex}
-                            select={(on) => {
-                                setActiveUnitIndex(on ? index : undefined)
-                            }}
-                            move={(position) => { handleMove(position, index) }}
-                            deleteUnit={() => { unitArray.deleteItem(index) }}
-                            setMarkers={(markers) => { unitArray.merge(index, markers) }}
-                        />
-                    ))}
-                    <Box>
-                        <Button variant="outlined" onClick={() => { setUnitDesignerOpen(true) }}>add unit</Button>
-                    </Box>
-                </Grid>
-            </Grid>
+        <Box>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Battle map
+                    </Typography>
+                    <Button color="inherit" onClick={() => { setBattleFieldDesignerOpen(true) }}>battle field</Button>
+                    <Button color="inherit" onClick={() => { setUnitDesignerOpen(true) }}>add unit</Button>
+                </Toolbar>
+            </AppBar>
+            <Container maxWidth={'lg'} sx={{ marginTop: 2 }}>
+                <Grid container>
+                    <Grid item xs={8}>
+                        <DownloadableSvgFrame
+                            reportClick={handleFrameClick}
+                            fileName="map.png" boxProps={{ border: '1px solid black', padding: 1 }} viewBox={battleField.viewBox}>
+                            <FloodRect fill={battleField.backgroundColor} viewBox={battleField.viewBox} />
+                            {units.map((unit, index) => (
+                                <UnitFigure key={index}
+                                    isActive={activeUnitIndex === index}
+                                    showMarkers
+                                    unit={unit} />
+                            ))}
+                        </DownloadableSvgFrame>
+                    </Grid>
+                    <Grid item xs={4} paddingX={1}>
 
-            <Dialog open={unitDesignerOpen} maxWidth="lg">
-                <DialogContent>
-                    <UnitDesigner confirm={handleConfirmDesign} badges={badges}/>
-                </DialogContent>
-            </Dialog>
-        </Container>
+                        {units.map((unit, index) => (
+                            <UnitControl
+                                key={index}
+                                unit={unit}
+                                index={index}
+                                activeIndex={activeUnitIndex}
+                                select={(on) => {
+                                    setActiveUnitIndex(on ? index : undefined)
+                                }}
+                                move={(position) => { handleMove(position, index) }}
+                                deleteUnit={() => { unitArray.deleteItem(index) }}
+                                setMarkers={(markers) => { unitArray.merge(index, markers) }}
+                            />
+                        ))}
+
+                        <Button fullWidth
+                            sx={{ marginTop: 1 }}
+                            variant={'outlined'}
+                            onClick={() => { setUnitDesignerOpen(true) }}>add unit</Button>
+                    </Grid>
+                </Grid>
+                <Dialog open={unitDesignerOpen} maxWidth="lg">
+                    <DialogContent>
+                        <UnitDesigner confirm={handleConfirmDesign} badges={badges} />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() => { setUnitDesignerOpen(false) }}>close</Button>
+                    </DialogActions>
+                </Dialog>
+                <Dialog open={battleFieldDesignerOpen} maxWidth="lg" onClose={() => { setBattleFieldDesignerOpen(false) }}>
+                    <DialogTitle>Battle Field</DialogTitle>
+                    <DialogContent>
+                        <BattleFieldDesigner
+                            battleField={battleField}
+                            setBattleField={setBattleField} />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() => { setBattleFieldDesignerOpen(false) }}>close</Button>
+                    </DialogActions>
+                </Dialog>
+            </Container>
+        </Box>
     )
 }
--- src/components/BattleFieldDesigner.tsx
+++ src/components/BattleFieldDesigner.tsx
@@ -21,7 +21,7 @@ export const BattleFieldDesigner = ({ battleField, setBattleField }: Props) => {
     const makeHandler = (property: keyof ViewBox): ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> => (event) => { changeProperty(property, event.target.value) }
 
     return (
-        <Stack direction={'row'} marginBottom={1}>
+        <Stack direction={'row'} marginTop={2}>
             <TextField label='height' type="number" value={battleField.viewBox.height} onChange={makeHandler('height')} />
             <TextField label='width' type="number" value={battleField.viewBox.width} onChange={makeHandler('width')} />
             <TextField sx={{ minWidth: 100 }} label='background' type="color" value={battleField.backgroundColor} onChange={ev => { setBattleField({ ...battleField, backgroundColor: ev.target.value }) }} />
