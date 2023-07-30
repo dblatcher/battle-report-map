a768060639315f26d073a967c462f0e807776cd4
dblatcher <dv.bltchr@gmail.com>
2023-07-30 10:58:09 +0100

    page setup with option to custom theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 'use client'
 import { AppMain } from "@/components/AppMain";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+
 
 export default function Home() {
   return (
-    <main >
-      <AppMain />
+    <main>
+      <ThemeProvider theme={theme}>
+        <AppMain />
+      </ThemeProvider>
     </main>
   )
 }
